3fe5ddb7a3ce3343a48a35e430e3819e31cdfb53
Wen Chen <wen@wormbase.org>
2018-10-11 13:21:18 -0700

    Add Gene Description

--- dumpWS_Gene_ace.sh
+++ dumpWS_Gene_ace.sh
@@ -17,6 +17,7 @@ setenv ACEDB /home/citace/WS/acedb/
 QUERY FIND Gene WBGene*
 show -a -t Identity -f ace_files/WBGeneIdentity.ace
 show -a -t Species -f ace_files/WBGeneSpe.ace
+show -a -t Structured_description -f ace_files/WBGeneDescription.ace
 show -a -t Molecular_info -f ace_files/WBGeneTranscript.ace
 show -a -t Contained_in_operon -f ace_files/WBGeneOperon.ace
 QUERY FIND Variation Variation_type = *allele; Sequenced
