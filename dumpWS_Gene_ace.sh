1e70739ce2815471fb8577b06e4cac2b0aa9907f
Wen Chen <wen@wormbase.org>
2019-10-29 10:15:07 -0700

    Remove dead genes
    Stop including dead genes from getting in SimpleMine since there are too many of them and the Status button creates confusion.

--- dumpWS_Gene_ace.sh
+++ dumpWS_Gene_ace.sh
@@ -7,7 +7,7 @@ cp /home/citace/WS/species/c_elegans/PRJNA13758/annotation/c_elegans.PRJNA13758.
 gunzip /home/wen/simpleMine/ace_files/ce_orthologs.txt.gz
 cp /home/citace/WS/species/c_elegans/PRJNA13758/annotation/c_elegans.PRJNA13758.WS*.interactions.txt.gz /home/wen/simpleMine/ace_files/ce_interaction.txt.gz
 gunzip /home/wen/simpleMine/ace_files/ce_interaction.txt.gz
-cp /home/citace/WS/ONTOLOGY/disease_association.WS*.wb /home/wen/simpleMine/ace_files/disease_association.wb
+#cp /home/citace/WS/ONTOLOGY/disease_association.WS*.wb /home/wen/simpleMine/ace_files/disease_association.wb
 cat /home/wen/AutoDescription/ecSummary/*.csv > /home/wen/simpleMine/ace_files/allECreg.csv
 
 #-------------prepare ace files for PCL file generation---------------------
@@ -15,7 +15,7 @@ cd /home/wen/simpleMine/
 setenv ACEDB /home/citace/WS/acedb/
 ## from Wen
 /usr/local/bin/tace -tsuser 'wen' <<END_TACE
-QUERY FIND Gene WBGene*
+QUERY FIND Gene WBGene*; Status = Live 
 show -a -t Identity -f ace_files/WBGeneIdentity.ace
 show -a -t Species -f ace_files/WBGeneSpe.ace
 show -a -t Structured_description -f ace_files/WBGeneDescription.ace
@@ -23,7 +23,7 @@ show -a -t Molecular_info -f ace_files/WBGeneTranscript.ace
 show -a -t Contained_in_operon -f ace_files/WBGeneOperon.ace
 QUERY FIND Variation Variation_type = *allele; Sequenced
 show -a -f ace_files/SequencedAllele.ace
-QUERY FIND Interaction predicted
+QUERY FIND Interaction Predicted AND Log_likelihood_score > 1.5 
 show -a -t Interactor_overlapping_gene -f ace_files/WSInt.ace
 QUERY FIND Gene Map_info = *
 show -a -t Map_info -f ace_files/WBGeneMap.ace
